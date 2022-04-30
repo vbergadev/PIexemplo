9317df524a8674ae092ee8a05cfede343405f8a4
Victor Bergamini <vbergdev@gmail.com>
2022-04-30 16:49:37 -0300

    add session and refactor routes

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const bcrypt = require('bcrypt');
 
 const connection = require("./database/database");
 
-const userModel = require("./database/models/User");
-
 const User = require("./database/models/User");
+
+const session = require('express-session');
 
 connection
     .authenticate()
@@ -29,21 +29,34 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(bodyParser.json()); // para APIs
 
+// Adicionando express session
+app.use(session({resave:true, saveUninitialized: true, secret: 'secret'}))
+
+// ROTAS 
 app.get("/",(req, res) => {
     res.render("./main/register.ejs");
 });
 app.get("/login",(req, res) => {
     res.render("./main/login.ejs");
 });
-app.get("/profile",(req, res) => {
+app.get("/profile",validaUsuario, (req, res) => {
     res.render("./main/profile.ejs");
 });
 
-app.post("/saveuser", async (req, res) => {
+app.get("/segura", validaUsuario, (req, res) => {
+    res.render("./main/profile.ejs");
+});
+
+app.post("/saveuser", saveUser);
+
+app.post("/authuser", login);
+
+// Controller
+async function saveUser (req, res) {
     // precisa do bodyParser
     const { name, email, age, password } = req.body;
     const saltRounds = 8;
-    const hashpassword = await bcrypt.hash(password, saltRounds)
+    const hashpassword = await bcrypt.hash(password, saltRounds);
     User.create({
         name: name,
         email: email,
@@ -56,23 +69,36 @@ app.post("/saveuser", async (req, res) => {
     })
 
     res.redirect("/login")
-});
-app.post("/authuser", async (req, res) => {
-    // precisa do bodyParser
+}
+
+async function login(req, res){
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email: email } });
     if (!user) {
         console.log('Not found!');
     } else {
-        const checkPassword = bcrypt.compare(password, user.password).then((result) => {
+        const checkPassword = await bcrypt.compare(password, user.password).then((result) => {
             return result
-        })
-        if(checkPassword && email == user.email) {         
+        });
+        if(checkPassword && email == user.email) {
+            req.session.user = email
             res.redirect("/profile")
         }
     }
 
-});
+}
+
+//Middleware de Rotas
+
+function validaUsuario(req, res, next) {
+    const user = req.session.user;
+    if(user) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
 
 app.listen(3000,()=>{console.log("O pai TA on!");});
